e682dcd4212175ba6073a13051746202fc021782
Aditya Cholkar <adityacholkar@gmail.com>
2023-04-05 21:05:35 +0530

    set up routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
-const App = () => <div>App</div>;
+const App = () => (
+  <div>
+    <CssBaseline />
+    <main>
+      <Routes>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/about">
+          <h1>About</h1>
+        </Route>
+      </Routes>
+    </main>
+  </div>
+);
 
 export default App;
