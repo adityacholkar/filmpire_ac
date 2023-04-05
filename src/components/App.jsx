dd923796be3d150095c28d2037eaf2c8e6dacc56
Aditya Cholkar <adityacholkar@gmail.com>
2023-04-05 21:55:18 +0530

    updated code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,8 @@ const App = () => (
     <CssBaseline />
     <main>
       <Routes>
-        <Route path="/">
-          <h1>Home</h1>
-        </Route>
-        <Route path="/about">
-          <h1>About</h1>
-        </Route>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
       </Routes>
     </main>
   </div>
