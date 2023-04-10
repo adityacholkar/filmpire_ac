7e31da2ab906e1b69b06652926008cdf24808efe
Aditya Cholkar <adityacholkar@gmail.com>
2023-04-10 09:00:22 +0530

    added navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+
+const NavBar = () => <div>NavBar</div>;
+
+export default NavBar;
